7516f9b707ab423e65fe5b604641febe0baf38f3
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-02-07 17:30:40 +0530

    New updated files

--- src/bank/classesTut.kt
+++ src/bank/classesTut.kt
@@ -1,4 +1,33 @@
 package bank
 
-class classesTut {
+open class Rectangle{
+    open fun draw(){
+        println("Drawing a rectangle")
+    }
+    val borderColor: String = "Red"
+}
+
+class FilledRectangle: Rectangle(){
+    override fun draw() {
+        val filler = Filler()
+        filler.drawAndFill()
+    }
+
+    inner class Filler{
+        fun fill(){
+            println("Filling")
+        }
+        fun drawAndFill(){
+            super@FilledRectangle.draw()
+            fill()
+            println("Drawn a filled rectangle")
+        }
+    }
+}
+
+
+fun main(){
+    val rec = FilledRectangle().Filler()
+    rec.fill()
+    rec.drawAndFill()
 }
