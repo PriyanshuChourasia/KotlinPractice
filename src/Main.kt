47bb5bf9b8ff17586a954b4238f29216b919b7d1
Priyanshu Chourasia <priyanshuchourasia916@gmail.com>
2025-02-25 20:29:38 +0530

    isnotempty update

--- src/Main.kt
+++ src/Main.kt
@@ -1,8 +1,8 @@
 fun main(){
-//    variable declared without initialization
-    val a:Int
-    a = 3
-    val name: String = "Hello World"
-    println(a)
-    println(name)
+
+    val a =""
+
+    println("IsNotEmpty ${a.isEmpty()}")
+    println("isNotBlank ${a.isBlank()}")
+    println("contain spaces ${a.contains("")}")
 }
